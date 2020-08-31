f59d92141ee43e9173bb9c8f430c5d4263678864
Shreddan <40694103+Shreddan@users.noreply.github.com>
2020-08-31 16:02:18 +0100

    Panning  - CHECK

--- OlcCodeJam2020/Engine.cpp
+++ OlcCodeJam2020/Engine.cpp
@@ -12,6 +12,10 @@ Engine::~Engine()
 bool Engine::OnUserCreate()
 {
 	srand(time(NULL));
+	pixelSX = 0;
+	pixelSY = 0;
+	//offsetX = ScreenWidth() / 2;
+	//offsetY = ScreenHeight() / 2;
 	currentMap = new Map(10, 10);
 	currentMap->setupRooms();
 	return true;
@@ -20,22 +24,41 @@ bool Engine::OnUserCreate()
 bool Engine::OnUserUpdate(float fElapsedTime)
 {
 	if (splash.AnimateSplashScreen(fElapsedTime)) return true;
-
+	splash.SetDuration(4.0f);
 	Clear(olc::BLACK);
+	inputHandler();
 	drawMap();
 	return true;
 }
 
-void Engine::WorldtoScreen(float worldX, float worldY, int screenX, int screenY)
+void Engine::WorldtoScreen(float worldX, float worldY, int &screenX, int &screenY)
 {
 	screenX = (int)(worldX - offsetX);
 	screenY = (int)(worldY - offsetY);
 }
 
-void Engine::ScreentoWorld(int screenX, int screenY, float worldX, float worldY)
+void Engine::ScreentoWorld(int screenX, int screenY, float &worldX, float &worldY)
 {
 	worldX = (float)(screenX + offsetX);
 	worldY = (float)(screenY + offsetY);
+}
+
+void Engine::inputHandler()
+{
+	if (GetMouse(0).bPressed)
+	{
+		PanX = GetMouseX();
+		PanY = GetMouseY();
+	}
+
+	if (GetMouse(0).bHeld)
+	{
+		offsetX -= (GetMouseX() - PanX);
+		offsetY -= (GetMouseY() - PanY);
+
+		PanX = GetMouseX();
+		PanY = GetMouseY();
+	}
 }
 
 void Engine::drawMap()
@@ -44,7 +67,8 @@ void Engine::drawMap()
 	{
 		for (int y = 0; y < currentMap->mapHeight; y++)
 		{
-			DrawRect(olc::vi2d((x * Room::roomSize) + roomGap, (y * Room::roomSize) + roomGap),olc::vi2d(Room::roomSize - roomGap, Room::roomSize - roomGap), olc::BLUE);
+			WorldtoScreen(x, y, pixelSX, pixelSY);
+			DrawRect(olc::vi2d((pixelSX * Room::roomSize) + roomGap, (pixelSY * Room::roomSize) + roomGap),olc::vi2d(Room::roomSize - roomGap, Room::roomSize - roomGap), olc::BLUE);
 		}
 	}
 }
--- OlcCodeJam2020/Engine.h
+++ OlcCodeJam2020/Engine.h
@@ -13,13 +13,23 @@ public:
 	bool OnUserCreate();
 	bool OnUserUpdate(float fElapsedTime);
 
-	void WorldtoScreen(float worldX, float worldY, int screenX, int screenY);
-	void ScreentoWorld(int screenX, int screenY, float worldX, float worldY);
+	void WorldtoScreen(float worldX, float worldY, int &screenX, int &screenY);
+	void ScreentoWorld(int screenX, int screenY, float &worldX, float &worldY);
+
+	void inputHandler();
 
 	void drawMap();
 
 	olcPGEX_SplashScreen splash;
 
+	int pixelSX;
+	int pixelSY;
+
+	float MouseX;
+	float MouseY;
+
+	float PanX;
+	float PanY;
 
 	float offsetX = 0.0f;
 	float offsetY = 0.0f;
